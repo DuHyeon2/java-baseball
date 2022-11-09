ce0d8d762948c10f36c71bb51bdcdd8afce27178
Kim Du Hyeon <83499405+DuHyeon2@users.noreply.github.com>
2022-11-09 17:54:20 +0900

    update

---
+++ src/main/java/baseball/CheckBall.java
@@ -0,0 +1,36 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckBall {
+
+    public static List<Integer> computerBall(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+    public static List<Integer> checkBall(List<Integer> myBall, List<Integer> computer) {
+        int strike = 0, ball = 0;
+        List<Integer> check = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            if (myBall.get(i) == computer.get(i)) {
+                strike++;
+            } else if (myBall.contains(computer.get(i))) {
+                ball++;
+            }
+        }
+        check.add(strike);
+        check.add(ball);
+        return check;
+    }
+    
+}
---
+++ src/main/java/baseball/RightBall.java
@@ -0,0 +1,27 @@
+package baseball;
+
+import java.util.List;
+
+public class RightBall {
+    public static int rightBall(List<Integer> check){
+        if(check.get(0) == 3){
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return 0;
+        }
+        if(check.get(0) == 0 && check.get(1) == 0){
+            System.out.println("낫싱");
+            return 0;
+        }
+        else {
+            if(check.get(1) > 0) {
+                System.out.print(check.get(1) + "볼 ");
+            }
+            if(check.get(0) > 0) {
+                System.out.print(check.get(0) + "스트라이크");
+            }
+            System.out.println("");
+            return 0;
+        }
+    }
+}
