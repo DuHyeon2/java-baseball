e25aba8d3f894ff5c8d6318645c50de9293b354d
Kim Du Hyeon <83499405+DuHyeon2@users.noreply.github.com>
2022-11-10 10:09:21 +0900

    update

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,20 +9,7 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Scanner sc = new Scanner(System.in);
-
-        CheckBall checkBall = new CheckBall();
-        RightBall rightBall = new RightBall();
-
-        String ball;
-        List<Integer> my_ball = new ArrayList<>();
-        System.out.println("숫자 야구 게임을 시작합니다");
-        System.out.print("숫자를 입력해주세요 : ");
-        ball = sc.next();
-        for(int i = 0; i < ball.length(); i++){
-            my_ball.add(i);
-        }
-        rightBall.rightBall(checkBall.checkBall(my_ball,checkBall.computerBall()));
+        GameStart gameStart = new GameStart();
+        gameStart.gameStart();
     }
-
 }
---
+++ src/main/java/baseball/GameStart.java
@@ -0,0 +1,35 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameStart {
+    public void gameStart(){
+        CheckBall checkBall = new CheckBall();
+        RightBall rightBall = new RightBall();
+        List<Integer> my_ball = new ArrayList<>();
+
+        System.out.println("숫자 야구 게임을 시작합니다");
+        System.out.print("숫자를 입력해주세요 : ");
+        String ball = Console.readLine();
+        for(int i = 0; i < ball.length(); i++){
+            my_ball.add(i);
+        }
+        rightBall.rightBall(checkBall.checkBall(my_ball,checkBall.computerBall()));
+//        restart();
+    }
+
+//    public void restart(){
+//        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
+//        String restart = Console.readLine();
+//        if(restart.equals("1")){
+//            gameStart();
+//        }
+//        else if (restart.equals("2")) {
+//            System.out.println("");
+//        }
+//    }
+}
+
